2d53a1913131f059156de0c810b41bc7a04b6e43
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-11-01 22:36:59 +0100

    feat: add function categories()
    add function categories()
    change getCategories(true) => getCategories(highlight)
    
    issue #5

--- App/src/controller/FrontController.php
+++ App/src/controller/FrontController.php
@@ -8,12 +8,21 @@ class FrontController extends Controller
 
     public function home()
     {
-        $title = 'Blog - Webaby';
         $articles = $this->articleDAO->getLastArticles(NB_LAST_ARTICLES);
-        $categories = $this->categoryDAO->getCategories(true);
+        $categories = $this->categoryDAO->getCategories('highlight');
         return $this->view->render($this->controller, 'home', [
            'articles' => $articles,
            'categories' => $categories
+        ]);
+    }
+
+    public function categories()
+    {
+        $categoriesHighlight = $this->categoryDAO->getCategories('highlight');
+        $categoriesActive = $this->categoryDAO->getCategories('active');
+        return $this->view->render($this->controller, 'categories', [
+           'categoriesHighlight' => $categoriesHighlight,
+           'categoriesActive' =>$categoriesActive
         ]);
     }
 }
