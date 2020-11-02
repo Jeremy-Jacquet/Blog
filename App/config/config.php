ee2fe02bb49e184ed27596bb2bac5018c143d44c
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-11-02 10:33:47 +0100

    feat: add defines
    add defines:
    - active_article = 1
    - inactive_article = 0
    - pending_article = null
    - main_category = 1
    - active_category = null
    - inactive_category = 0
    
    issue #7

--- App/config/config.php
+++ App/config/config.php
@@ -3,3 +3,11 @@
 define("URL", (isset($_SERVER['HTTPS']) ? "https" : "http")."://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?route=');
 
 define("NB_LAST_ARTICLES", 4);
+
+define("ACTIVE_ARTICLE", 1);
+define("INACTIVE_ARTICLE", 0);
+define("PENDING_ARTICLE", NULL);
+
+define("MAIN_CATEGORY", 1);
+define("ACTIVE_CATEGORY", NULL);
+define("INACTIVE_CATEGORY", 0);
