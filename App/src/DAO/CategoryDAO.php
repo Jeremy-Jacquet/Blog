ac5e9d0e7056ca788cacf578e622f48246caa33b
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-11-08 11:41:54 +0100

    feat: add existsCategory()
    add:
    - existsCategory($id)
    translate connection => connexion
    
    issue #9

--- App/src/DAO/CategoryDAO.php
+++ App/src/DAO/CategoryDAO.php
@@ -4,6 +4,7 @@ namespace App\src\DAO;
 
 use App\src\blogFram\DAO;
 use App\src\entity\Category;
+use \PDO;
 
 class CategoryDAO extends DAO
 {
@@ -22,11 +23,11 @@ class CategoryDAO extends DAO
     {
         if($status == null) {
             $sql = 'SELECT * FROM category WHERE status IS NULL';
-            $result = $this->checkConnection()->query($sql);
+            $result = $this->checkConnexion()->query($sql);
         } else{
             $sql = 'SELECT * FROM category WHERE status = :status';
-            $result = $this->checkConnection()->prepare($sql);
-            $result->bindValue(':status', $status, \PDO::PARAM_INT);
+            $result = $this->checkConnexion()->prepare($sql);
+            $result->bindValue(':status', $status, PDO::PARAM_INT);
         }
         $result->execute();
         $categories = [];
@@ -41,13 +42,24 @@ class CategoryDAO extends DAO
     public function getCategory($id)
     {
         $sql = 'SELECT * FROM category WHERE (status = 1 OR status IS NULL) AND id = :id';
-        $result = $this->checkConnection()->prepare($sql);
-        $result->bindValue(':id', $id, \PDO::PARAM_INT);
+        $result = $this->checkConnexion()->prepare($sql);
+        $result->bindValue(':id', $id, PDO::PARAM_INT);
         $result->execute();
         $row = $result->fetch();
         $category = $this->buildObject($row);
         $result->closeCursor();
         return $category;
+    }
+
+    public function existsCategory($id)
+    {
+        $sql = 'SELECT title FROM category WHERE id = :id';
+        $result = $this->checkConnexion()->prepare($sql);
+        $result->bindValue(':id', $id, PDO::PARAM_INT);
+        $result->execute();
+        $exists = $result->fetch();
+        $result->closeCursor();
+        return ($exists) ? true : false;
     }
 
 }
