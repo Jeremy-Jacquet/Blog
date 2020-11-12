4a94b62d725b0b4f7a46118918f7e7a74550d567
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-11-12 16:59:23 +0100

    feat: create Comment.php
    create:
    - App/src/entity/Comment.php
    add:
    - getters
    - setters
    
    issue #12

---
+++ App/src/entity/Comment.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\src\entity;
+
+class Comment
+{
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $content;
+
+        /**
+     * @var int
+     */
+    private $articleId;
+
+    /**
+     * @var int
+     */
+    private $userId;
+
+    /**
+     * @var string
+     */
+    private $createdAt;
+
+    /**
+     * @var int
+     */
+    private $status;
+
+    /*
+    |--> GETTERS <--|
+    */
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return int
+     */
+    public function getArticleId()
+    {
+        return $this->articleId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreated()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /*
+    |--> SETTERS <--|
+    */
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @param int $articleId
+     */
+    public function setArticleId($articleId)
+    {
+        $this->articleId = $articleId;
+    }
+
+    /**
+     * @param int $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @param string $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @param int $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+}
