199f14c2e573084dd14c8116cb3c1cdc6904d539
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-11-13 15:59:11 +0100

    feat: add logout()
    add:
    - public function logout()
    
    issue #11

--- App/src/controller/BackController.php
+++ App/src/controller/BackController.php
@@ -10,9 +10,24 @@ class BackController extends Controller
     private $controller = 'back';
     private $errors = [];
 
+    public function logout()
+    {
+        $this->session->stop();
+        $this->session->start();
+        $this->session->set('logout', 'À bientôt');
+        header("Location: ".URL."accueil");
+    }
+
+    public function profile()
+    {
+        if($this->checkLoggedIn()) {
+            return $this->view->render($this->controller, 'profile');
+        }
+    }
+
     public function dashboard()
     {
-        // if(isAdmin) {
+        if($this->checkAdmin()) {
             $pendingArticles = Search::lookForOr($this->articleDAO->getArticles(),[
                 'status' => PENDING_ARTICLE
             ]);
@@ -20,19 +35,18 @@ class BackController extends Controller
                 'status' => PENDING_COMMENT
             ]);
             $users = $this->userDAO->getUsers();
-            $admin = $this->userDAO->getUser(1); // $admin = $this->userDAO->getUser($_SESSION['id']);
+            $admin = $this->userDAO->getUser($_SESSION['id']);
             return $this->view->render($this->controller, 'dashboard', [
                 'admin' => $admin,
                 'users' => $users,
                 'pendingArticles' => $pendingArticles,
                 'pendingComments' => $pendingComments
             ]);
-        /* 
         } else {
+            $this->session->set('not_admin', 'Vous n\'avez pas le droit d\'accéder à cette page');
             header("Location: ".URL."accueil");
             exit;
         }
-        */
     }
 
 }
