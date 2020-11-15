a567f66b53d81e16b5bd36a7691eff1e6fe1c17c
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-11-15 10:35:08 +0100

    feat: create Alert
    create:
    - App/src/blogFram/Alert
    add:
    - addError()
    - addSuccess()
    - checkAlert()
    - checkError()
    - checkSuccess()
    - getError()
    - getSuccess()
    - showError()
    - showSuccess()
    
    issue #52

---
+++ App/src/blogFram/Alert.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\src\blogFram;
+
+class Alert
+{
+    private $session;
+    private $error;
+    private $success;
+
+    public function __construct()
+    {
+        $this->session = new Session($_SESSION);
+    }
+
+    public function addError($message)
+    {
+        $this->error[] = $message;
+        $this->session->set('error', $this->error);
+    }
+
+    public function addSuccess($message)
+    {
+        $this->success[] = $message;
+        $this->session->set('success', $this->success);
+    }
+
+    public function checkAlert()
+    {
+        if($this->getSuccess() OR $this->getError()) {
+            return true;
+        }
+    }
+
+    public function checkError()
+    {
+        if($this->getError()) {
+            return true;
+        }
+    }
+
+    public function checkSuccess()
+    {
+        if($this->getSuccess()) {
+            return true;
+        }
+    }
+
+    public function getError()
+    {
+        $errors = [];
+        if($this->session->get('error')) {   
+            foreach($this->session->get('error') as $key => $message){
+                $errors[] = $message;
+            }         
+        }
+        return $errors;
+    }
+
+    public function getSuccess()
+    {
+        $successes = [];
+        if($this->session->get('success')) {
+            foreach($this->session->get('success') as $key => $message){
+                $successes[] = $message;
+            }
+        }
+        return $successes;
+    }
+
+    public function showError()
+    {
+        $errors = $this->getError();
+        if($errors) {
+            $this->session->remove('error');
+            foreach($errors as $key => $message){
+                echo '<p class="text-center  m-auto">'.$message.'</p>';
+            }
+        }
+    }
+    public function showSuccess()
+    {
+        $successes = $this->getSuccess();
+        if($successes) {
+            $this->session->remove('success');
+            foreach($successes as $key => $message){
+                echo '<p class="text-center  m-auto">'.$message.'</p>';
+            }
+        }
+    }
+}
