748a9f072bbdb2de6c05f4301a5a50326996f1a5
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-11-21 10:27:14 +0100

    feat: create ImageValidation
    create:
    - App/src/constraint/ImageValidation
    
    issue #13

---
+++ App/src/constraint/ImageValidation.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\src\constraint;
+
+use App\src\blogFram\Alert;
+
+class ImageValidation extends Validation
+{
+    private $constraint;
+    private $alert;
+    private $imagePath;
+
+    const SIZE_MAX_AVATAR = 200000;                // Taille max en octets du fichier
+    const WIDTH_MAX_AVATAR = 800;                  // Largeur max de l'image en pixels
+    const HEIGHT_MAX_AVATAR = 800;                 // Hauteur max de l'image en pixels
+
+    public function __construct()
+    {
+        $this->constraint = new ImageConstraint();
+        $this->alert = new Alert();
+    }
+
+    /*
+    // nom du fichier
+    // type du fichier
+    // taille du fichier
+    // taille autorisée du serveur (php.ini)
+    // nom temporaire
+    // dossier temporaire
+    // existence => renommage si besoin
+    // code erreur
+    */
+
+    public function checkAvatar($file, $directory)
+    {
+        $validate = $this->checkImageUpload(
+            $file, 
+            $directory,
+            self::SIZE_MAX_AVATAR, 
+            self::HEIGHT_MAX_AVATAR, 
+            self::WIDTH_MAX_AVATAR
+        );
+        return (!$validate)? false : true;
+    }
+
+    public function checkImageUpload($file, $directory, $sizeMax, $heightMax, $widthMax) {
+        $error = 0;
+        if($this->checkDirectory($directory)) {
+            $this->alert->addError($this->checkDirectory($directory));
+            $error = +1;
+        }
+        if($this->checkImageTrue($file)) {
+            $this->alert->addError($this->checkImageTrue($file));
+            $error = +1;
+        }
+        if($this->checkName($file)) {
+            $this->alert->addError($this->checkName($file));
+            $error = +1;
+        }
+        if($this->checkExtension($file)) {
+            $this->alert->addError($this->checkExtension($file));
+            $error = +1;
+        }
+        if($this->checkImageSize($file, $sizeMax, $heightMax, $widthMax)) {
+            $this->alert->addError($this->checkImageSize($file, $sizeMax, $heightMax, $widthMax));
+            $error = +1;
+        }
+        if($this->checkError($file)) {
+            $this->alert->addError($this->checkError($file));
+            $error = +1;
+        }
+        return ($error)? false : true;  
+    }
+
+    public function checkDirectory($directory)
+    {
+        if($this->constraint->checkDirectory($directory)) {
+            return $this->constraint->checkDirectory($directory);
+        }
+    }
+
+    public function checkImageTrue($file) {
+        if($this->constraint->checkImageTrue($file)) {
+            return $this->constraint->checkImageTrue($file);
+        }
+        
+    }
+
+    public function checkName($file) {
+        if($this->constraint->checkName($file)) {
+            return $this->constraint->checkName($file);
+        }
+    }
+
+    public function checkExtension($file) {
+        if($this->constraint->checkExtension($file)) {
+            return $this->constraint->checkExtension($file);
+        }
+    }
+
+    public function checkImageSize($file, $sizeMax, $heightMax, $widthMax) {
+        if($this->constraint->checkImageSize($file, $sizeMax, $heightMax, $widthMax)) {
+            return $this->constraint->checkImageSize($file, $sizeMax, $heightMax, $widthMax);
+        }
+    }
+
+    public function checkError($file) {
+        if($this->constraint->checkError($file)) {
+            return $this->constraint->checkError($file);
+        }
+    }
+
+}
