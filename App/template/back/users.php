7b0117e5813eb2e22f2271f27681bfb89dd0707f
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-11-23 11:26:34 +0100

    feat: add delete form to users.php
    add delete form to users.php
    
    issue #15

--- App/template/back/users.php
+++ App/template/back/users.php
@@ -5,9 +5,15 @@
         <p><?= $user->getPseudo(); ?></p>
         <p><?= $user->getFlag(); ?></p>
         <p><?= ($user->getBanned() == 0)? 'Accepté' : 'Banni'; ?></p>
-        <form method="post" action="<?= URL ?>admin&categorie=membres">
+        <form method="post" action="">
             <input type="hidden" name="id" value="<?= $user->getId(); ?>">
             <input type="submit" name="submit" value="Modifier l'utilisateur">
+        </form>
+        <form method="post" action="">
+            <input type="checkbox" name="deleteConfirm">
+            <input type="hidden" name="delete" value="true">
+            <input type="hidden" name="id" value="<?= $user->getId(); ?>">
+            <input type="submit" name="submit" value="Supprimer le membre">
         </form>
     </div>
     <?php } ?>
