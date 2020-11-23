233fc5184ea202e112eeaa5cb7396b02d155114f
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-11-23 11:24:42 +0100

    nav: add profile route to nav
    add:
    - <a href="<?= URL ?>profil">Profil</a>
    
    issue #15

--- App/template/include/nav.php
+++ App/template/include/nav.php
@@ -3,5 +3,6 @@
 <a href="<?= URL ?>articles">Articles</a>
 <a href="<?= URL ?>inscription">Inscription</a>
 <a href="<?= URL ?>connexion">Connexion</a>
+<a href="<?= URL ?>profil">Profil</a>
 <a href="<?= URL ?>deconnexion">Deconnexion</a>
 <a href="<?= URL ?>admin">Administration</a>
