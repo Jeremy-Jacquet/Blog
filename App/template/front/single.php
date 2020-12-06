b3cf44bba9d437ac4213a4b9c555722bf4fe0ba0
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-12-06 10:38:53 +0100

    feat: make the form disappear if not connected
    - make the form disappear if not connected
    - show the error at the top of the page with the link to the comment to modify
    
    issue #76

--- App/template/front/single.php
+++ App/template/front/single.php
@@ -1,3 +1,9 @@
+<?= 
+($this->alert->checkErrorComment())? 
+$this->alert->showErrorComment() . '<p class="text-center  m-auto"><a href="#comment">Modifier votre commentaire</a></p>' 
+: ''; 
+?>
+
 <section id="single">
     <div class="row">
         <div class="border border-dark col-6 mx-auto my-3 p-3">
@@ -10,31 +16,47 @@
             <p><?= $article->getUserPseudo(); ?></p>
             <p><?= $article->getPublishedAt(); ?></p>
             <img src="img/article/<?= $article->getFilename(); ?>" alt="">
-            <a href="#comment">Ecrire un commentaire</a>
-        </div>
-    </div>
-</section>
-
-
-
-<section id="comment">
-    <div class="border border-dark col-6 mx-auto my-3 p-3">
-        <div>
-            <?php if(!$this->session->get('id')) { ?>
-                <p>Attention: vous devez être inscrit et connecté pour accéder à cette fonctionnalité.</p>
+            <?php if($this->session->get('id')) { ?>
+                <a href="#comment">Ecrire un commentaire</a>
+            <?php } else { ?>
+                <p>Pour laisser un commentaire veuillez vous inscrire et vous connecter.</p>
             <?php } ?>
-            <form action="<?= URL ?>articles&action=commenter&id=<?= $article->getId(); ?>" method="post">
-                <label for="comment">Laissez un commentaire</label>
-                <br>
-                <?= ($this->alert->checkErrorComment())? $this->alert->showErrorComment() : ''; ?>
-                <textarea id="comment" name="comment" rows="5" cols="33">
-                    <?= ($comment)? $comment : ''; ?>
-                </textarea>
-                <br>
-                <input type="hidden" name="articleId" value="<?= $article->getId(); ?>">
-                <input type="hidden" name="userId" value="<?= $this->session->get('id'); ?>">
-                <input type="submit" name="submit" value="Envoyer le commentaire">
-            </form>
         </div>
     </div>
 </section>
+
+<?php if($comments){ ?>
+    <section id="comment">
+        <?php foreach($comments as $comment) { ?>
+            <div class="border border-dark col-6 mx-auto my-3 p-3">
+                <p>
+                    <img src="img/avatar/<?= $article->getFilename(); ?>" alt="">
+                    <?= $users[$comment->getUserId()]->getPseudo(); ?>
+                </p>
+                <p>
+                    <?= $comment->getContent(); ?>
+                </p>
+            </div>
+        <?php } ?>
+    </section>
+<?php } ?>
+
+<?php if($this->session->get('id')) { ?>
+    <section id="comment">
+        <div class="border border-dark col-6 mx-auto my-3 p-3">
+            <div>
+                <form action="" method="post">
+                    <label for="comment">Laissez un commentaire</label>
+                    <br>
+                    <textarea id="content" name="content" rows="5" cols="33">
+                        <?= ($content)? $content : ''; ?>
+                    </textarea>
+                    <br>
+                    <input type="hidden" name="articleId" value="<?= $article->getId(); ?>">
+                    <input type="hidden" name="userId" value="<?= $this->session->get('id'); ?>">
+                    <input type="submit" name="submit" value="Envoyer le commentaire">
+                </form>
+            </div>
+        </div>
+    </section>
+<?php } ?>
