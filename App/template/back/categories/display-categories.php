c1abf9a8cfc4420d18e191f6fdfe29ff56f52025
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-12-11 19:56:34 +0100

    feat: add link to update category
    add link to update category
    
    issue #21

--- App/template/back/categories/display-categories.php
+++ App/template/back/categories/display-categories.php
@@ -4,6 +4,7 @@
             <img src="img/category/<?= $category->getFilename(); ?>" alt="">
             <p><?= $category->getTitle(); ?></p>
             <p><?= $category->getSentence(); ?></p>
+            <a href="<?= URL ?>admin&categorie=categories&action=modifier&id=<?= $category->getId(); ?>">Modifier</a>
         </div>
     <?php } ?>
 </section>
